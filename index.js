6cab1f86dfc77d95327be7f7adf5b27c6b1f61c3
Pavitra <pavitragolchha@gmail.com>
2020-03-07 19:59:18 +0530

    Fix package

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const core = require("@actions/core");
 const {context, GitHub} = require("@actions/github");
 
 async function run() {
-    const html_file = core.getInput("html_file");
-    const {owner, repo} = context.repo;
+    try {
+        const html_file = core.getInput("html_file");
+        const {owner, repo} = context.repo;
 
-    const {GITHUB_TOKEN} = process.env;
-    const client = new GitHub(GITHUB_TOKEN);
+        const {GITHUB_TOKEN} = process.env;
+        const client = new GitHub(GITHUB_TOKEN);
 
-    await client.issues.createComment({
-        owner, repo,
-        issue_number: context.payload.pull_request.number,
-        body: `[Preview](https://htmlpreview.github.io/?https://github.com/${owner}/${repo}/blob/${context.ref}/${html_file})`
-    });
+        await client.issues.createComment({
+            owner, repo,
+            issue_number: context.payload.pull_request.number,
+            body: `[Preview](https://htmlpreview.github.io/?https://github.com/${owner}/${repo}/blob/${context.ref}/${html_file})`
+        });
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
-run().catch(core.error);
+run();
