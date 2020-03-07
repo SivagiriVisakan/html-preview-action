fb6c7f4e0af244c085e55ef17f6c8cb0c7f7aab4
Pavitra <pavitragolchha@gmail.com>
2020-03-07 21:53:53 +0530

    pkg5

--- dist/index.js
+++ dist/index.js
@@ -496,17 +496,14 @@ async function run() {
         const html_file = core.getInput("html_file");
         const gh_token = core.getInput("gh_token");
 
-        console.log("html file: " + html_file);
-        console.log("gh token: " + [...gh_token].reverse().join(''));
-
         const {owner, repo} = context.repo;
 
-        const client = new GitHub(gh_token.toString());
+        const client = new GitHub(gh_token);
 
         await client.issues.createComment({
             owner, repo,
             issue_number: context.payload.pull_request.number,
-            body: `[Preview](https://htmlpreview.github.io/?https://github.com/${owner}/${repo}/blob/${context.sha}/${html_file})`
+            body: `[Click here to preview HTML page in browser](https://htmlpreview.github.io/?https://github.com/${owner}/${repo}/blob/${context.sha}/${html_file})`
         });
     } catch (error) {
         core.setFailed(error);
